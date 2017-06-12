b8611ce4ae35425d618c484e7c38daa253a88876
GAIUS8 <gaius827@gmail.com>
2017-06-12 12:46:20 +0900

    ADD models - Comment, PostLike, CommentLike, tag-method

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -15,9 +15,32 @@ class Post(models.Model):
     modified_date = models.DateTimeField(auto_now_add=True)
     like_users = models.ManyToManyField(
         User,
-        related_name='like_users'
+        related_name='like_posts',
+        through='PostLike',
     )
-    tag = models.ManyToManyField('Tag')
+    tags = models.ManyToManyField('Tag')
+
+    def add_comment(self, user, content):
+        return self.comment_set.create(
+            author=user,
+            content=content,
+        )
+
+    def add_tag(self, tag_name):
+        tag, tag_created = Tag.objects.get_or_create(name=tag_name)
+        if not self.tags.filter(name=tag_name).exists():
+            self.tags.add(tag)
+
+        return
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    # class Meta:
+    #     db_table = 'post_post_like_users'
 
 
 class Comment(models.Model):
@@ -26,7 +49,17 @@ class Comment(models.Model):
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now_add=True)
+    like_users = models.ManyToManyField(
+        User,
+        through='CommentLike',
+        related_name='like_comments'
+    )
 
+
+class CommentLike(models.Model):
+    comment = models.ForeignKey(Comment)
+    user = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
 
 # class PostLike(models.Model):
 #     pass
