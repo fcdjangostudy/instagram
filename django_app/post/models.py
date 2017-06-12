8290c4b6d738c68af99750dcdbe5bb1cb5904c60
GAIUS8 <gaius827@gmail.com>
2017-06-12 11:27:37 +0900

    ADD models - Post, Comment, Tag

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -5,19 +5,35 @@ member application생성
 이후 해당 User모델을 Post나 Comment에서 author나 user항목으로 참조
 """
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    pass
+    author = models.ForeignKey(User)
+    photo = models.ImageField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True)
+    like_users = models.ManyToManyField(
+        User,
+        related_name='like_users'
+    )
+    tag = models.ManyToManyField('Tag')
 
 
 class Comment(models.Model):
-    pass
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(User)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True)
 
 
-class PostLike(models.Model):
-    pass
+# class PostLike(models.Model):
+#     pass
 
 
 class Tag(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Tag ({})'.format(self.name)
